07c3376e2efcadfd1f7e459e8b4d7c0b56a7558d
Nebuchadnezzar <110613468+Nebuchadnazzer@users.noreply.github.com>
2024-03-14 07:21:55 +0800

    Added operate function

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ const multiply = function (x, y) {
 const divide = function (x, y) {
     return x / y;
 }
+
+const operate = function (operator, variableX, variableY) {
+    switch (operator) {
+        case '+':
+            add(variableX, variableY);
+            break;
+        case '-':
+            subtract(variableX, variableY);
+            break;
+        case '*':
+            multiply(variableX, variableY);
+            break;
+        case '/':
+            divide(variableX, variableY);
+            break;
+    }
+}
